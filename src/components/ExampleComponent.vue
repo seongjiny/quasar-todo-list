60aada05a5b7a5d0c74e4d4dba38a7a553e932e8
seongjiny <yoon.0423@gmail.com>
2022-12-12 18:10:51 +0900

    WIP todo list design

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -1,37 +1,68 @@
 <template>
-  <div>
-    <p>{{ title }}</p>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id" @click="increment">
-        {{ todo.id }} - {{ todo.content }}
-      </li>
-    </ul>
-    <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
-    <p>Active: {{ active ? 'yes' : 'no' }}</p>
-    <p>Clicks on todos: {{ clickCount }}</p>
+  <div class="wide-container">
+    <h3>TODO it!</h3>
+    <div class="inline">
+      <div class="input-div">
+        <q-input
+          input-class="text-center"
+          filled
+          v-model="newItem"
+          placeholder="Type What you have to do"
+        />
+      </div>
+      <div class="add-div">
+        <q-btn class="add-btn" icon="add" stack glossy color="purple" />
+      </div>
+    </div>
+    <div class="card-list">
+      <q-card>
+        <q-card-section class="card-text">
+          <q-icon name="check" />
+          abc
+          <q-btn :class="`no-shadow`" icon="delete" />
+        </q-card-section>
+      </q-card>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { Todo, Meta } from './models';
+import { ref } from 'vue';
 
-interface Props {
-  title: string;
-  todos?: Todo[];
-  meta: Meta;
-  active: boolean;
-}
-const props = withDefaults(defineProps<Props>(), {
-  todos: () => [],
-});
-
-const clickCount = ref(0);
-function increment() {
-  clickCount.value += 1;
-  return clickCount.value;
-}
-
-const todoCount = computed(() => props.todos.length);
-
+const newItem = ref('');
 </script>
+
+<style lang="scss" scoped>
+h3 {
+  text-align: center;
+}
+.wide-container {
+  width: 100%;
+  margin: 20px;
+}
+.inline {
+  display: flex;
+  width: 100%;
+  height: 50px;
+}
+.input-div {
+  flex: 1;
+  text-align: center;
+}
+.add-div {
+  width: 50px;
+  .add-btn {
+    height: 50px;
+  }
+}
+
+div.card-list {
+  margin-top: 20px;
+  .q-icon {
+    margin-right: 20px;
+  }
+  .q-btn {
+    box-shadow: none !important;
+  }
+}
+</style>
